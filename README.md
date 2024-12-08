36e607a8ec09314cf0580197a63386962ef8b761
HongKiBum <hgb9720@hanyang.ac.kr>
2024-12-08 17:20:17 +0900

    add sample file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Alcohol_Games_Sameple_repository
+
+
+#pypi 링크
+https://pypi.org/project/Alcohol-Total-Game/
+
+
+
+#readthedocs 링크
+https://alcohol-game-total.readthedocs.io/ko/latest/
--- Sample_test.py
+++ Sample_test.py
@@ -6,17 +6,18 @@ tg = total_game()
 tg.Receipt()
 
 #취한 정도 파악 게임
+# (음성파일은 반드시 웨이브 파일로 업로드 해야합니다.)
+# ex) pronunciation.wav
 tg.PronunciationApp()
-(음성파일은 반드시 웨이브 파일로 업로드 해야합니다.)
-ex) pronunciation.wav
+
 
 #사진 벌칙 게임
 tg.GroupPhotoAnalyzerApp()
 
 #사진 기반 인물 퀴즈
 images = [
-    {"image": "path_to_image_1.jpg", "answer": "Person 1"},
-    {"image": "path_to_image_2.jpg", "answer": "Person 2"}
+    {"image": "IGJ.jpg", "answer": "Person 1"},
+    {"image": "hanyang.jpg", "answer": "Person 2"}
 ]
 tg.GuesseGameApp(images)
 
